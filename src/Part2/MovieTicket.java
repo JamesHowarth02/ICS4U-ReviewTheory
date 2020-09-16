0be6ff640044678fc776b3eb16fea52d53ee0187
jame1850 <jame1850@ISS2TNRKH2.scdsb.pri>
2020-09-16 12:00:12 -0400

    Created If, Loop, MovieTicket and PopulationGrowth.

--- src/Part2/MovieTicket.java
+++ src/Part2/MovieTicket.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Part2;
 
-/**
- *
- * @author jame1850
- */
+import java.util.Scanner;
+
 public class MovieTicket {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        Scanner s = new Scanner(System.in);
+        String day;
+        int age;
+        double price;
+        boolean ageok;
+
+        while (true) {
+            System.out.print("Enter day code: \"m\"-Matinee, \"t\"-Tuesday \"o\"-Other: ");
+            day = s.next().toLowerCase();
+            if("o".equals(day) || "m".equals(day) || "t".equals(day)) break;
+            System.out.println("Error, must be o, t, or m. Please try again.\n");
+        }
+        switch (day) {
+            case "m":
+                price = 5;
+                break;
+            case "t":
+                price = 4;
+                break;
+            default:
+                do{
+                    System.out.println("Enter age from 1 to 99.");
+                    age = s.nextInt();
+                }while(age < 1 || age > 99);
+                price = age <18? 8 : 10;
+                break;
+        }
+        
+        System.out.println("Price is: " + price);
+    }    
 }
