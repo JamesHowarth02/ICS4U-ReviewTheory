6f22219c3d963c6c512f1fc05d652b4c41f92f4c
jame1850 <jame1850@ISS2TNRKH2.scdsb.pri>
2020-09-17 09:13:38 -0400

    - Added ErrorTypes

---
+++ src/Part1/ErrorTypes.java
@@ -0,0 +1,30 @@
+package Part1;
+
+
+public class ErrorTypes {
+
+    public static void main(String[] args) {
+       /*
+            3 Types of Errors
+            Syntax: --> Error in the "spelling".
+                int x = 45 --> No semicolon. 
+            Runtime: --> Program begins but crashes or throws an error later on in the program.
+                Scanner s = new Scanner(System.in);
+                int Candy = 100;
+                int howmany;
+                System.out.print("How many people are you giving candy to? > ");
+                howmany = s.nextInt();
+                int eachgets = candy/howmany // Could crash or throw an error depending on the answer to this line.
+                System.out.println("Each person gets " + eachgets);
+            Logic: --> Program works properly but something is wrong with it. Hardest issue to detect in a program.
+                System.out.println("Enter wage/hr > ");
+                double wage = s.nextDouble();
+                System.out.println("Enter hours worked: ");
+                double hours = s.nextDouble();
+                double pay = wage + hours;
+                System.out.println("Your pay is " + pay);
+        
+       */
+       
+    }
+}
